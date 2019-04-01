6439b563be5896e03dc8e6fd6816706dc8273c8e
Vimal Soni <vimal.h.soni@gmail.com>
2019-04-01 23:02:25 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -13,11 +13,10 @@ class ChattyStudent < Student
   def raise_hand
     super
     super
-      super
-      super
-      super
-      super
+    super
+    super
+    super
+    super
   end
-end
 
 end
